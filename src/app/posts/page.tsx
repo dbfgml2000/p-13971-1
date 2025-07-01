4b12fc5471608d53156e80fbc4f0b035b9743a67
dbfgml20 <dbfgml20@gmail.com>
2025-07-01 14:56:09 +0900

    스프링부트 서버와 연동

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,17 +3,17 @@
 import { useEffect, useState } from "react";
 
 export default function Page() {
-    const [posts, setPosts] = useState<{id: number, title: string}[]>([]);
+    const [posts, setPosts] = useState<{[key: string]: any}[]>([]);
 
     useEffect(() => {
-        setPosts([
-            {id: 1, title: "명언 1"},
-            {id: 2, title: "명언 2"},
-        ]);
+        fetch("http://localhost:8080/api/v1/posts")
+        .then((res)=>res.json())
+        .then(setPosts)
     }, []);
 
   return <>
     <h1>글 목록</h1>
+    {posts.length == 0 && <div>로딩중...</div>}
     <ul>
         {posts.map((post) => (
             <li key={post.id}>{post.title}</li>
