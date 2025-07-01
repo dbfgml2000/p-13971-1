b23953737829edff90ce6209bd0f14b373a103f7
dbfgml20 <dbfgml20@gmail.com>
2025-07-01 12:43:48 +0900

    a 대신 Link 사용해 속도 향상

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,8 @@ export default function RootLayout({
       >
         <header>
           <nav className="flex">
-            <a href="/" className="p-2 rounded hover:bg-gray-100">메인</a>
-            <a href="/posts" className="p-2 rounded hover:bg-gray-100">글 목록</a>
+            <Link href="/" className="p-2 rounded hover:bg-gray-100">메인</Link>
+            <Link href="/posts" className="p-2 rounded hover:bg-gray-100">글 목록</Link>
           </nav>
         </header>
         <main>{children}</main>
